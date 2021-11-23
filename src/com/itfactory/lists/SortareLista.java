28c66f282f92e6a7dde15f3d9a55824fd11af712
drtrainig <91653979+drtrainig@users.noreply.github.com>
2021-11-23 10:07:26 +0200

    Update SortareLista.java

--- src/com/itfactory/lists/SortareLista.java
+++ src/com/itfactory/lists/SortareLista.java
@@ -7,17 +7,16 @@ import java.util.List;
 public class SortareLista {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(2);
-        list.add(2);
-
-
-        List<Integer> list1 = new ArrayList<>();
-        list1.addAll(list);
-        list1.addAll(list);
-        list1.addAll(list);
-        list1.addAll(list);
-
-        System.out.println(list1);
+        list.add(6);
+        list.add(4);
+        list.add(1);
+        
+        System.out.print("Lista nesortata: ");
+        System.out.println(list);
+        
+        Collections.sort(list);
+        
+        System.out.print("Lista sortata: ");
+        System.out.println(list);
     }
 }
